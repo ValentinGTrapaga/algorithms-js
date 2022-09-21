14d05683715fb9fa2e00cb53bbdff2248985882e
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-09-21 00:54:44 -0300

    TOProject exercise completed and chapter read

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -1,6 +1,6 @@
-const mergeSort = require('./mergeSort')
+import { mergeSort } from './mergeSort.js'
 
-class Node {
+export class Node {
   constructor(data, leftNode = null, rightNode = null) {
     this.data = data
     this.right = rightNode
@@ -8,7 +8,7 @@ class Node {
   }
 }
 
-class Tree {
+export class Tree {
   constructor(arr) {
     this.root = this.buildTree(arr)
   }
@@ -80,9 +80,9 @@ class Tree {
     }
   }
 
-  inOrderTraverse(callback) {
+  inOrderTraverse(arr) {
     if (this.root === null) return null
-    this.inOrderTraverseNode(this.root, callback)
+    this.inOrderTraverseNode(this.root, arr)
   }
 
   preOrderTraverse(callback) {
@@ -93,6 +93,138 @@ class Tree {
   postOrderTraverse(callback) {
     if (this.root === null) return null
     this.postOrderTraverseNode(this.root, callback)
+  }
+
+  levelOrderTraverse(callback) {
+    if (this.root === null) return null
+    let queue = [this.root]
+    let currentNode
+    while (queue.length !== 0) {
+      currentNode = queue.shift() // array.shift() gets the first value from the queue
+      callback(currentNode.data)
+      if (currentNode.left !== null) queue.push(currentNode.left)
+      if (currentNode.right !== null) queue.push(currentNode.right)
+    }
+  }
+
+  minNode(node) {
+    let current = node
+    while (current !== null && current.left !== null) {
+      // Viaja hasta el nodo de mas a la izquierda
+      current = current.left
+    }
+    return current
+  }
+
+  min() {
+    return this.minNode(this.root)
+  }
+
+  maxNode(node) {
+    let current = node
+    while (current !== null && current.right !== null) {
+      // Viaja hasta el nodo de mas a la derecha
+      current = current.right
+    }
+    return current
+  }
+
+  max() {
+    return this.maxNode(this.root)
+  }
+
+  searchNode(node, key) {
+    if (node === null) return false
+    if (key < node.data) {
+      return this.searchNode(node.left, key)
+    } else if (key > node.data) {
+      return this.searchNode(node.right, key)
+    } else {
+      return node
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value)
+  }
+
+  removeNode(node, value) {
+    if (node === null) return null // {2}
+    if (value < node.data) {
+      // {3}
+      node.left = this.removeNode(node.left, value) // {4}
+      return node // {5}
+    } else if (value > node.data) {
+      // {6}
+      node.right = this.removeNode(node.right, value) // {7}
+      return node // {8}
+
+      //We found the node!!!
+    } else {
+      // key is equal to node.item
+      // case 1
+      if (node.left == null && node.right == null) {
+        // {9}
+        // leaf node
+        node = null // {10} Assigns null to the node found
+        return node // {11} Returns the null value to assign it to the parent
+      }
+      // case 2 node with left or right child
+      if (node.left == null) {
+        // {12}
+        node = node.right // {13}
+        return node // {14}
+      } else if (node.right == null) {
+        // {15}
+        // it has no right child so the one on the left is assigned as the new node
+        node = node.left // {16}
+        return node // {17}
+      }
+      //case 3
+      const aux = this.minNode(node.right) // {18}
+      node.data = aux.data // {19}
+      node.right = this.removeNode(node.right, aux.data) // {20}
+      return node // {21}
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value)
+  }
+
+  height(node = this.root) {
+    if (node === null) {
+      return -1
+    } else {
+      let left = this.height(node.left)
+      let right = this.height(node.right)
+
+      return Math.max(left, right) + 1
+    }
+  }
+
+  isBalanced(root = this.root) {
+    if (root === null) return false
+    let leftHalfTree = root.left
+    let rightHalfTree = root.right
+
+    let difference = Math.abs(
+      this.height(leftHalfTree) - this.height(rightHalfTree)
+    )
+    if (difference > 1) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  rebalance() {
+    if (this.isBalanced(this.root) == false) {
+      let treeArray = []
+      this.levelOrderTraverse((elem) => treeArray.push(elem))
+      let newTree = new Tree(treeArray)
+      this.root = newTree.root
+    }
   }
 }
 
@@ -112,17 +244,35 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 const tree = new Tree([
   1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 325, 15, 27
 ])
-console.log(tree.root)
-console.log(tree.root.left)
-console.log(tree.root.right)
+// console.log(tree.root)
+// console.log(tree.root.left)
+// console.log(tree.root.right)
+console.log(tree.isBalanced())
 console.log(prettyPrint(tree.root))
 tree.insert(25)
 tree.insert(28)
 tree.insert(280)
 tree.insert(6)
+tree.insert(665)
+tree.insert(6234523)
+tree.insert(6123123)
+tree.insert(6432)
+tree.insert(1231236)
+tree.insert(567886)
+tree.insert(6123)
+tree.insert(67)
+tree.insert(69)
+tree.insert(-92)
+tree.insert(-8934)
+tree.insert(-8921)
+// console.log(prettyPrint(tree.root))
+// tree.remove(67)
 console.log(prettyPrint(tree.root))
-tree.inOrderTraverse(console.log)
-console.log('----------------------')
-tree.preOrderTraverse(console.log)
-console.log('----------------------')
-tree.postOrderTraverse(console.log)
+// let inOrderArr = []
+// tree.inOrderTraverse((elem) => inOrderArr.push(elem))
+// tree.levelOrderTraverse((elem) => inOrderArr.push(elem))
+// console.log(inOrderArr)
+console.log(tree.isBalanced())
+tree.rebalance()
+console.log(tree.isBalanced())
+console.log(prettyPrint(tree.root))
--- mergeSort.js
+++ mergeSort.js
@@ -20,12 +20,10 @@ function merge(leftArr, rightArr) {
   ]
 }
 
-function mergeSort(arr) {
+export function mergeSort(arr) {
   if (arr.length < 2) return arr
 
   const half = arr.length / 2
   const left = arr.splice(0, half)
   return merge(mergeSort(left), mergeSort(arr))
 }
-
-module.exports = mergeSort
